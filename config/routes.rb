8b0317985fffbba866b2db169dbcc0b9907e8cdd
Matthew Rivera <matthewjesseissac@gmail.com>
2018-10-02 16:41:15 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'pokemon/index'
+  get 'pokemon/show'
+  get 'aerodactyl' => 'pokemon#aerodactyl'
+  get 'dragonite' => 'pokemon#dragonite'
+  get 'pikachu' => 'pokemon#pikachu'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
